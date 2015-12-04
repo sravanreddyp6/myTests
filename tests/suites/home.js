579f954375ea301fb9d4bb51a57ab1b46708abda
Dang Mai <Dang.Mai@TheMentorNetwork.com>
2015-12-04 09:45:22 -0500

    EC-304 - Stop displaying previous entries on disregarded evaluations

---
+++ tests/suites/home.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var assert = chai.assert;
+var testSuite = require("../main.js").testSuite;
+var users = require("../users.js").accounts;
+
+var suiteTimeout = 2 * 60 * 1000;
+var defaultOperationTimeout = 30 * 1000;
+
+testSuite("Home Page", suiteTimeout, {
+  "should go to the correct page": function(client, done) {
+    return client
+      .logInAs(users["CM_Tier_I"])
+      .url()
+      .then(function (url) {
+        assert.include(url.value, 'Home');
+      });
+  }
+});
