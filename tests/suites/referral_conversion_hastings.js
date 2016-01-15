ac961d74529a49cc3d7dbb6f5ea9ffdff3a5a799
Dang Mai <Dang.Mai@TheMentorNetwork.com>
2016-01-15 11:09:09 -0500

    RT - Initial detailed referral conversion tests

---
+++ tests/suites/referral_conversion_hastings.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const testSuite = require("../main.js").testSuite;
+const users = require("../users.js").accounts;
+
+const helper = require("../helpers/referral_conversion.js");
+
+const suiteTimeout = 5 * 60 * 1000;
+const defaultOperationTimeout = 30 * 1000;
+
+testSuite("Referral Conversion for Hastings", suiteTimeout, {
+  "should have correct Convert button visibility on different Referral statuses": function(client, done) {
+    return helper.testConvertButtonVisibility(client, "Cambridge", "GA");
+  },
+});
