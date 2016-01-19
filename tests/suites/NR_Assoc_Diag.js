3b51b047de13d16dd39e90df03eef6933dac023c
melnikd <melnikd@localhost>
2016-01-19 15:03:19 -0500

    Regression Testing: All Test Cases for Diagnosis for NR

--- tests/suites/NR_Assoc_Diag.js
+++ tests/suites/NR_Assoc_Diag.js
@@ -7,6 +7,7 @@ var fs   = require('fs');
 var GaRefPa = JSON.parse(stripJsonComments(fs.readFileSync("./configs/GaReferralPage.json", "utf8")));
 var suiteTimeout = 10 * 60 * 1000;
 var defaultOperationTimeout = 3 * 60 * 1000;
+var diagn = [{"Action": "Edit", "ICD-10 Code":"A01.02","Code Type":"","ICD/DSM-VDescription/Axis-IV Description":"Typhoid fever with heart involvement", "Date and Time of Diagnosis": "12/31/2015 15:00", "Status": "Active", "Type": ""}];
 //Should cover Test Case: Associate/Disassociate Diagnosis to Service Assignment 1-7 for NR
 //Should cover Test Case: View Diagnosis 3,4, and 6 for NR
 testSuite("NR_Assoc_Diag", suiteTimeout, {
@@ -14,11 +15,12 @@ testSuite("NR_Assoc_Diag", suiteTimeout, {
   var user = users["HS_AL_Auburn_Referral_Intaker"];
   var today = new Date().getMilliseconds() + new Date().getDate();
     return client
-      .execUtil("create_referral", {
+      .execUtil("convert_referral", {
       operatingGroup: "NeuroRestorative",
-      flavor: "MA"
-      })
-      .isExisting("input[value='PRE 10/1/2015']")
+      flavor: "MA",
+      hooks: {
+      		"convert_referral_initial_referral": function (client) {
+      		return client.isExisting("input[value='PRE 10/1/2015']")
       .then(function (isExisting) {
       		if(isExisting==false)
       	return true;
@@ -58,18 +60,83 @@ testSuite("NR_Assoc_Diag", suiteTimeout, {
       .waitForActionStatusDisappearance("AdddiagStatus", defaultOperationTimeout)
       //.waitForExist("span[id$=AdddiagStatus] img[class=dialogLoadingSpinner]", 2000, true)
       .refresh()
-      .waitForVisible("input[value='Convert']", defaultOperationTimeout)
-      .click("input[value='Convert']")
-      .waitForVisible("input[value='Confirm Conversion']", defaultOperationTimeout)
-      .click("input[value='Confirm Conversion']")
-      .click("a[title='ESD Home Tab']")
-      .click("a=My Recently Viewed Persons Being Served")
-      .waitForVisible("input[value='Refresh']", defaultOperationTimeout)
-      .click("table#persons_table tbody tr:nth-child(1) td:nth-child(3) a")
+        	}
+        }
+      })
+      
+      //.waitForVisible("input[value='Confirm Conversion']", defaultOperationTimeout)
+      //.click("input[value='Confirm Conversion']")
+      //.click("a[title='ESD Home Tab']")
+      //.click("a=My Recently Viewed Persons Being Served")
+      //.waitForVisible("input[value='Refresh']", defaultOperationTimeout)
+      //.click("table#persons_table tbody tr:nth-child(1) td:nth-child(3) a")
       //.element("#resultsFrame")
       //.then(function (frame) { return frame.value; })
       //.then(client.frame)
       //.click("tr.dataRow th a:first-child")
+      .waitForVisible("input[value='Add Diagnosis']", defaultOperationTimeout)
+      .scroll("input[value='Add Diagnosis']", 0, -500)
+      .tableToJSON("[id$=diagTable]")
+      .then(function (icd10code) {
+        assert.deepEqual(diagn, icd10code);
+      })
+      .isExisting("input[value='PRE 10/1/2015']")
+      .then(function (isExisting) {
+      		if(isExisting==false)
+      	return true;
+      		else
+      	return this.click("input[value='PRE 10/1/2015']");
+      })
+      .click("input[value='Add Diagnosis']")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+      .waitForVisible("span[id$=diagnosisModal] input[value='Save']", defaultOperationTimeout)
+	  .getSelectOptionsBySelector("[id$=diagnosisEntry_status]")
+	  .then(function(digstat) {
+        assert.deepEqual([
+          "", "Active", "Inactive", "Void" ], digstat);
+      })
+      .getSelectOptionsBySelector("[id$=diagnosisEntry_type]")
+	  .then(function(digtype) {
+        assert.deepEqual([
+          "", "Admission", "Discharge", "Onset", "Update"  ], digtype);
+      })
+      
+      .click("a[id$=diagnosisEntry_icd_lkwgt]")
+      //.selectLookup("ICD-10 Code")
+      .switchToNextWindow()
+      .element("#searchFrame")
+      .then(function (frame) { return frame.value; })
+      .then(client.frame)
+      .setValue("input#lksrch", "01")
+      .click("input[value*='Go']")
+      .frameParent()
+      .waitForExist("#resultsFrame", defaultOperationTimeout)
+      .element("#resultsFrame")
+      .then(function (frame) { return frame.value; })
+      .then(client.frame)
+      .click("tr.dataRow th a:first-child")
+      .switchToNextWindow()
+      .waitForVisible("span[id$=diagnosisModal] input[value='Save']", defaultOperationTimeout)
+      .fillInputText("Date and Time of Diagnosis", "12/30/2015 12:00")
+      .selectByValue("select[id$=diagnosisEntry_status]", "Active")
+      .selectByValue("select[id$=diagnosisEntry_type]", "Admission")
+      .click("span[id$=diagnosisModal] input[value='Save']")
+	  .waitForVisible("input[value='Edit Person Being Served']", defaultOperationTimeout)
+	  .waitForActionStatusDisappearance("saveResponseStatus", defaultOperationTimeout)
+	  .waitForActionStatusDisappearance("AdddiagStatus", defaultOperationTimeout)
+	  .refresh()
+      //.waitForExist("input[value='PRE 10/1/2015']",defaultOperationTimeout,true)
+      //.then(function (el) {
+      //		if(el==true)
+      //	return true;
+      //		else
+      //	return this.click("input[value='PRE 10/1/2015']");
+      //})
+      .click("a*=Admission 1 -")
+      .waitForVisible("input[value='Edit Admission']", defaultOperationTimeout)
+      .refresh()
+      .scroll("input[value='New Assessment Only']", 0, -500)
+      .click("a*=N. MA - SA1 - 130201")
       .waitForVisible("input[value='Associate Diagnosis']", defaultOperationTimeout)
       .scroll("input[value='Associate Diagnosis']", 0, -500)
 	  .click("input[value='Associate Diagnosis']")
@@ -77,33 +144,96 @@ testSuite("NR_Assoc_Diag", suiteTimeout, {
 	  .waitForVisible("span[id$=saDiagModal] input[value='Save']", defaultOperationTimeout)
 	  .getSelectOptionsBySelector("[id$=pbsdiagSelectList]", "true")
       .then(function(pbsdiaglist) {
-          assert.deepEqual(["--None--", "A01.01 - Typhoid meningitis"], pbsdiaglist);
+          assert.deepEqual(["--None--", "A01.02 - Typhoid fever with heart involvement", "A01.01 - Typhoid meningitis"], pbsdiaglist);
       })
 	  .getSelectOptionsBySelector("[id$=sadiagJoEntry_sadiagJoRanking]")
       .then(function(sadiagJoEntry) {
           assert.deepEqual(["", "Primary", "Secondary", "Tertiary" ], sadiagJoEntry);
       })
-      .getSelectOptions("Injury Type")
-      .then(function(injurytype) {
-          assert.deepEqual(["", "Acquired Brain Injury - Anoxia/hypoxia", "Acquired Brain Injury -- CVA -- R hemiplegia",
-          "Acquired Brain Injury -- CVA -- L hemiplegia", "Acquired Brain Injury -- TBI", "Acquired Brain Injury -- Other",
-          "Cognitive Disorder", "CONGENTIAL", "Medical - Cardiac", "Medical - GI", "Medical - GU",
-          "Medical - Cancer", "Neuro - Degenerative", "Neuro -Non-degenerative", "PTSD", 
-          "Pulmonary - End-stage", "Pulmonary - Other", "Orthopedic - Disease", "Orthopedic - Injury",
-          "Spinal Cord Injury -C 1-3", "Spinal Cord Injury -C4-5", "Spinal Cord Injury -C6-8",
-          "Spinal COrd Injury - Paraplegia", "Other"], injurytype);
-      })
-      .getSelectOptionsBySelector("Glasgow Coma Score at Injury")
-      .then(function(glasgowcomainjury) {
-          assert.deepEqual(["", "3-8", "9-12", "13-15" ], glasgowcomainjury);
-      })
-      .getSelectOptionsBySelector("Glasgow Coma Scale Severity")
-      .then(function(glasgowcomasev) {
-          assert.deepEqual(["", "Severe", "Moderate", "Mild" ], glasgowcomasev);
-      })
       
       .chooseSelectOption("Diagnosis", "A01.01 - Typhoid meningitis", "true")
       .selectByValue("select[id$=sadiagJoEntry_sadiagJoRanking]", "Primary")
+      .getSelectOptions("Injury Type")
+      .then(function(injurytype) {
+        var dash = String.fromCharCode("8211");
+          assert.deepEqual(["", "Acquired Brain Injury - Anoxia/hypoxia", "Acquired Brain Injury " + dash + " CVA " + dash + " R hemiplegia",
+          "Acquired Brain Injury " + dash + " CVA " + dash + " L hemiplegia", "Acquired Brain Injury " + dash + " TBI", "Acquired Brain Injury " + dash + " Other",
+          "Cognitive Disorder", "CONGENTIAL", "Medical - Cardiac", "Medical - GI", "Medical - GU",
+          "Medical - Cancer", "Neuro - Degenerative", "Neuro - Non-degenerative", "PTSD", 
+          "Pulmonary - End-stage", "Pulmonary - Other", "Orthopedic - Disease", "Orthopedic - Injury",
+          "Spinal Cord Injury - C 1-3", "Spinal Cord Injury - C4-5", "Spinal Cord Injury - C6-8",
+          "Spinal Cord Injury - Paraplegia", "Other"], injurytype);
+      })
+      .getSelectOptions("Glasgow Coma Score at Injury")
+      .then(function(glasgowcomainjury) {
+          assert.deepEqual(["", "3-8", "9-12", "13-15" ], glasgowcomainjury);
+      })
+      .getSelectOptions("Glasgow Coma Scale Severity")
+      .then(function(glasgowcomasev) {
+          assert.deepEqual(["", "Mild", "Moderate", "Severe"], glasgowcomasev);
+      })
+      
+      .chooseSelectOption("Injury Type", "PTSD")
+      .selectCheckbox("ABI Diagnosis")
+      .fillInputText("Injury Date", "12/25/2015")
+      .selectCheckbox("Billable")
+      .chooseSelectOption("Glasgow Coma Score at Injury", "3-8")
+      .chooseSelectOption("Glasgow Coma Scale Severity", "Severe")
+      .click("span[id$=saDiagModal] input[value='Save']")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+      .refresh()
+      .click("a=Edit")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+	  .waitForVisible("span[id$=saDiagModal] input[value='Save']", defaultOperationTimeout)
+	  .getSelectOptionsBySelector("[id$=pbsdiagSelectList]", "true")
+      .then(function(pbsdiaglist) {
+          assert.deepEqual(["--None--", "A01.02 - Typhoid fever with heart involvement", "A01.01 - Typhoid meningitis"], pbsdiaglist);
+      })
+	  .getSelectOptionsBySelector("[id$=sadiagJoEntry_sadiagJoRanking]")
+      .then(function(sadiagJoEntry) {
+          assert.deepEqual(["", "Primary", "Secondary", "Tertiary" ], sadiagJoEntry);
+      })
+      
+      .chooseSelectOption("Diagnosis", "A01.01 - Typhoid meningitis", "true")
+      .selectByValue("select[id$=sadiagJoEntry_sadiagJoRanking]", "Secondary")
+      .selectCheckbox("Billable")
+      .click("span[id$=saDiagModal] input[value='Save']")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+      .refresh()
+      .click("input[value='Associate Diagnosis']")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+	  .waitForVisible("span[id$=saDiagModal] input[value='Save']", defaultOperationTimeout)
+	  .getSelectOptionsBySelector("[id$=pbsdiagSelectList]", "true")
+      .then(function(pbsdiaglist) {
+          assert.deepEqual(["--None--", "A01.02 - Typhoid fever with heart involvement", "A01.01 - Typhoid meningitis"], pbsdiaglist);
+      })
+	  .getSelectOptionsBySelector("[id$=sadiagJoEntry_sadiagJoRanking]")
+      .then(function(sadiagJoEntry) {
+          assert.deepEqual(["", "Primary", "Secondary", "Tertiary" ], sadiagJoEntry);
+      })
+      
+      .chooseSelectOption("Diagnosis", "A01.02 - Typhoid fever with heart involvement", "true")
+      .selectByValue("select[id$=sadiagJoEntry_sadiagJoRanking]", "Primary")
+      .getSelectOptions("Injury Type")
+      .then(function(injurytype) {
+        var dash = String.fromCharCode("8211");
+          assert.deepEqual(["", "Acquired Brain Injury - Anoxia/hypoxia", "Acquired Brain Injury " + dash + " CVA " + dash + " R hemiplegia",
+          "Acquired Brain Injury " + dash + " CVA " + dash + " L hemiplegia", "Acquired Brain Injury " + dash + " TBI", "Acquired Brain Injury " + dash + " Other",
+          "Cognitive Disorder", "CONGENTIAL", "Medical - Cardiac", "Medical - GI", "Medical - GU",
+          "Medical - Cancer", "Neuro - Degenerative", "Neuro - Non-degenerative", "PTSD", 
+          "Pulmonary - End-stage", "Pulmonary - Other", "Orthopedic - Disease", "Orthopedic - Injury",
+          "Spinal Cord Injury - C 1-3", "Spinal Cord Injury - C4-5", "Spinal Cord Injury - C6-8",
+          "Spinal Cord Injury - Paraplegia", "Other"], injurytype);
+      })
+      .getSelectOptions("Glasgow Coma Score at Injury")
+      .then(function(glasgowcomainjury) {
+          assert.deepEqual(["", "3-8", "9-12", "13-15" ], glasgowcomainjury);
+      })
+      .getSelectOptions("Glasgow Coma Scale Severity")
+      .then(function(glasgowcomasev) {
+          assert.deepEqual(["", "Mild", "Moderate", "Severe" ], glasgowcomasev);
+      })
+      
       .chooseSelectOption("Injury Type", "PTSD")
       .selectCheckbox("ABI Diagnosis")
       .fillInputText("Injury Date", "12/25/2015")
@@ -124,6 +254,27 @@ testSuite("NR_Assoc_Diag", suiteTimeout, {
   }
   return res;
 })
+	   .then(function (el) { return client.elementIdText(el.value); })
+       .then(function (pad) {
+        assert.equal("1) A01.01-Typhoid meningitis", pad );
+        })
+      .click("a*=View Admission")
+      .waitForVisible("input[value='Add Diagnosis']", defaultOperationTimeout)
+      .scroll("input[value='Add Diagnosis']", 0, -500)
+      .click("a=Edit")
+      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
+      .waitForVisible("span[id$=diagnosisModal] input[value='Save']", defaultOperationTimeout)
+      .getSelectOptionsBySelector("[id$=diagnosisEntry_status]")
+	  .then(function(digstat) {
+        assert.deepEqual([
+          "", "Active", "Inactive", "Void" ], digstat);
+      })
+      .selectByValue("select[id$=diagnosisEntry_status]", "Void")
+      .selectByValue("select[id$=diagnosisEntry_type]", "Admission")
+	  .waitForVisible("span[id$=diagnosisModal] input[value='Save']", defaultOperationTimeout)
+	  .click("span[id$=diagnosisModal] input[value='Save']")
+	  isExisting("li*=You cannot inactivate this Diagnosis as it is associated with an Active Service Assignment.")
+
        .then(function (el) { return client.elementIdText(el.value); })
        .then(function (pad) {
         assert.equal("1) A01.01-Typhoid meningitis", pad );
