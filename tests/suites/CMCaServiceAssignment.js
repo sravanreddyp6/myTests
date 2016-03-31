c4bc0f73ff96a2ed865c25b82971c2e8b9dad496
Amruth Kollipara <contractor.amruth.kollipara@thementornetwork.com>
2016-03-31 10:45:58 -0400

    Shaun-107

--- tests/suites/CMCaServiceAssignment.js
+++ tests/suites/CMCaServiceAssignment.js
@@ -8,97 +8,17 @@ var defaultOperationTimeout = 30 * 1000;
 
 testSuite("CMCaServiceAssignment", suiteTimeout, {
   "should Create/Edit/View/Close/promptFullDischarge a Care Meridian CA Service Assignment successfully": function(client, done) {
-    var user = users["CM_DON"];
-    var today = new Date().getMilliseconds() + new Date().getDate();
-    return client
-      .logInAs(user)
-      .click("a=Create New Referral")
-      .waitForVisible("input[value='Create Person Being Referred']", defaultOperationTimeout)
-      .fillInputText("First Name", "Darth" + today)
-      .chooseSelectOption("Race", "Caucasian")
-      .fillInputText("Middle Name", "Freakin" + today)
-      .chooseSelectOption("Ethnicity", "Unknown")
-      .fillInputText("Last Name", "Vader" + today)
-      .chooseSelectOption("Marital Status", "Divorced")
-      .fillInputText("Date of Birth", "7/7/1970")
-      .chooseSelectOption("Highest Level of Education", "Graduate School")
-      .chooseSelectOption("Gender", "Male")
-      .fillInputText("Additional Information / Comments", "Really hateful")
-      .chooseSelectOption("Mailing State/Province", "California")
-      .click("input[value='Create Person Being Referred']")
-      .waitForVisible("input[value='Save Referral']", defaultOperationTimeout)
-      .fillInputText("Anticipated Admission DateTime", "12/30/2015 16:00") 
-      .click("input[value='Add Related Party']")
-      .waitForVisible("span[id$=relatedPartyModal]", defaultOperationTimeout)
-      .fillInputText("Party Name", "Anakin Skywalker")
-      .chooseSelectOption("Type", "Family/Friends")
-      .click("span[id$=relatedPartyModal] input[value=Save]")
-      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
-      .fillInputText("Referral Source", "Mentor")
-      .fillInputText("Referrer Name", "Obi-wan Kennobi")
-      .fillInputText("Current Location", "Boston")
-      .selectLookup("Evaluated By")
-      .switchToNextWindow()
-      .waitForExist("#searchFrame", defaultOperationTimeout)
-      .element("#searchFrame")
-      .then(function (frame) { return frame.value; })
-      .then(client.frame)
-      .setValue("input#lksrch", user["first_name"] + " " + user["last_name"])
-      .click("input[value*='Go']")
-      .frameParent()
-      .waitForExist("#resultsFrame", defaultOperationTimeout)
-      .element("#resultsFrame")
-      .then(function (frame) { return frame.value; })
-      .then(client.frame)
-      .click("#TMN_User__c_body tr.dataRow th a")
-      .switchToNextWindow()
-      .click("a[id$=originlookup]")
-      .waitForVisible("input[id$=originstate]", defaultOperationTimeout)
-      .setValue("input[id$=originstate]","CA")
-      .click("span[id$=searchDialog2] input[value='Search!']")
-      .waitForVisible("span[id$=searchDialog2] a", defaultOperationTimeout)
-      .element("span[id$=searchDialog2] a")
-      .then(function (el) {
-      return this.elementIdClick(el.value.ELEMENT);
-      })
-      .click("input[value='Add Location']")
-      .waitForVisible("span[id$=ReferralLocationModal] input[value='Save']", defaultOperationTimeout)
-      .chooseSelectOption("Rank", "Primary")
-      .selectByValue("select[id$=locationEntry_Status]", "Active")
-      .click("a[id$=aliaslookup]")
-      .waitForVisible("input[value='First']", defaultOperationTimeout)
-      .setValue("input[id$=addlocationstate]","CA")
-      .click("span[id$=searchDialog] input[value='Search!']")
-      .waitForVisible("span[id$=searchDialog] a", defaultOperationTimeout)
-      .element("span[id$=searchDialog] a")
-      .then(function (el) {
-      	return this.elementIdClick(el.value.ELEMENT);
-      })
-      .waitForVisible("span[id$=ReferralLocationModal] input[value='Save']", defaultOperationTimeout)
-      .click("span[id$=ReferralLocationModal] input[value='Save']")
-      .waitForActionStatusDisappearance("myStatus", defaultOperationTimeout)
-	  .waitForVisible("input[value='Add Funding Source']", defaultOperationTimeout)
-      .click("input[value='Add Funding Source']")
-      .waitForVisible("span[id$=FundingSourceModal]", defaultOperationTimeout)
-      .chooseSelectOption("Coverage Level", "Primary")	
-      .selectCheckbox("More than 1.5 Yrs of Disability")
-      .selectCheckbox("ALS/ESRD/Black Lung Disease")
-      .selectCheckbox("Patient Over 64 Years of Age")
-      .click("span[id$=FundingSourceModal] input[value='Save']")
-      .waitForActionStatusDisappearance("saveFundingSourceStatus", defaultOperationTimeout)
-      
-      .click("input[value='Save Referral']")      
-      .waitForVisible("input[value='Convert']", defaultOperationTimeout)
-      .click("input[value='Convert']")
-      .waitForVisible("span[id$=ReferralAdmissionLocationModal] input[value='Save and Continue']", defaultOperationTimeout)
-      .click("span[id$=ReferralAdmissionLocationModal] input[value='Save and Continue']")
-      .waitForVisible("input[value='Confirm Conversion']", defaultOperationTimeout)
-      .click("input[value='Confirm Conversion']")
-      
-      .waitForVisible("input[value='Edit Person Being Served']", defaultOperationTimeout)
-      .scroll("[id$=adminsId]", 0 , -300)
-      .click("table[id$=adminsId] tbody tr:nth-child(1) td:nth-child(2) a")     
-      
+	 return client
+	  //Referral Conversion
+	  .execUtil("convert_referral", {
+		  operatingGroup: "Care Meridian",
+	      flavor: "CA"
+	   })
+	   //Navigating to Admission Page
+	   .scroll("[id$=adminsId]", 0 , -300)
+	   .click("table[id$=adminsId] tbody tr:nth-child(1) td:nth-child(2) a")  
+	   .windowHandleMaximize()  
+	      
        //Service Assignment Regression Starts From here 
        // Test case 15: Cancel Service Assignment
       .waitForVisible("input[value='New Standard Service']", defaultOperationTimeout)
@@ -112,7 +32,7 @@ testSuite("CMCaServiceAssignment", suiteTimeout, {
       .scroll("input[value='New Standard Service']", 0 , -300)
       .click("input[value='New Standard Service']")      
       .waitForVisible("input[value='Save']", defaultOperationTimeout)
-      .fillInputText("Start Date", "01/04/2016 15:00") 
+      .fillInputText("Start Date", "01/13/2016 17:00") 
       .click(".lookupIcon")
       .waitForVisible("input[value='Search!']", defaultOperationTimeout)
       .setValue("input[id$=nameFilter]","114020")
@@ -157,7 +77,7 @@ testSuite("CMCaServiceAssignment", suiteTimeout, {
       .scroll("input[value='New Standard Service']", 0 , -300)
       .click("input[value='New Standard Service']")      
       .waitForVisible("input[value='Save']", defaultOperationTimeout)
-      .fillInputText("Start Date", "12/30/2015 16:00") 
+      .fillInputText("Start Date", "01/13/2016 17:00") 
       .click(".lookupIcon")
       .waitForVisible("input[value='Search!']", defaultOperationTimeout)
       .setValue("input[id$=nameFilter]","114020")
@@ -178,7 +98,7 @@ testSuite("CMCaServiceAssignment", suiteTimeout, {
           "", "Yes", "No"
         ], sEnd);
       })
-      .fillInputText("End Date", "1/5/2016") 
+      .fillInputText("End Date", "1/14/2016") 
       .chooseSelectOption("Was dissatisfaction the reason for service ending?", "No")
       .click("span[id$=buttons] input[value='Save']")
       .waitForVisible("input[value='Edit']", defaultOperationTimeout)
@@ -202,7 +122,7 @@ testSuite("CMCaServiceAssignment", suiteTimeout, {
           "", "Yes", "No"
         ], sEnd);
       })
-      .fillInputText("End Date", "1/5/2016") 
+      .fillInputText("End Date", "1/14/2016") 
       .chooseSelectOption("Was dissatisfaction the reason for service ending?", "No")
       .click("span[id$=buttons] input[value='Save']")
 	  .waitForVisible("input[value='No']", defaultOperationTimeout) 
@@ -218,7 +138,7 @@ testSuite("CMCaServiceAssignment", suiteTimeout, {
       })
       .getOutputText("End Date")
       .then(function (enddate) {
-        assert.equal("1/5/2016", enddate.trim());
+        assert.equal("1/14/2016", enddate.trim());
       })
       .getOutputText("Was dissatisfaction the reason for service ending?")
       .then(function (rservEnd) {
