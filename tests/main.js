633c3fc179b9bb248509f6d481852c2c4d12ff59
Dang Mai <Dang.Mai@TheMentorNetwork.com>
2016-05-13 14:47:21 -0400

    RT - Add Job system

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,61 @@
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 
 chai.Should();
 chai.use(chaiAsPromised);
 
-var webdriverio = require("webdriverio");
-var path = require("path");
-var options = {
+const webdriverio = require("webdriverio");
+const path = require("path");
+const options = {
     host: "127.0.0.1",
     desiredCapabilities: {
         browserName: "chrome"
     }
 };
-var defaultOperationTimeout = 30 * 1000;
 
+const errorHandler = function (name, client, done) {
+  return function (err) {
+    var ssFileName = path.normalize("./screenshots/Error - " + name + " - " + new Date().getTime() + ".png");
+    client
+      .saveScreenshot(ssFileName)
+      .then(function () {
+        if (err instanceof Error) {
+          if (err.message.substr(-1) === " " || err.message.substr(-1) === ".") {
+            err.message = err.message.slice(0, -1);
+          }
+          err.message += ". Screenshot saved to \"" + ssFileName + "\"";
+        }
+        if (done) {
+          done(err);
+        }
+      });
+  };
+}
 module.exports = {
+  execJobs: function (jobs) {
+    var client = webdriverio.remote(options).init();
+    var errorFound = false;
+
+    chaiAsPromised.transferPromiseness = client.transferPromiseness;
+    require("./commands.js")(client);
+
+    Object.keys(jobs).forEach(function (jobName) {
+      client = client
+        .then(function () { console.log("Running job: " + jobName); });
+      client = jobs[jobName](client)
+        .deleteCookie()
+        .then(undefined, errorHandler(jobName, client, function () {
+          errorFound = true;
+        }));
+    });
+    client
+      .end()
+      .then(function () {
+        if (errorFound) {
+          process.exit(2);
+        }
+      });
+  },
   testSuite: function (name, timeout, tests) {
     describe(name, function () {
       this.timeout(timeout);
@@ -30,20 +71,6 @@ module.exports = {
       Object.keys(tests).forEach(function (testName) {
         it(testName, function (done) {
           testsRun += 1;
-          var errorHandler = function (err) {
-            var ssFileName = path.normalize("./screenshots/Error - " + testName + " - " + new Date().getTime() + ".png");
-            client
-              .saveScreenshot(ssFileName)
-              .then(function () {
-                if (err instanceof Error) {
-                  if (err.message.substr(-1) === " " || err.message.substr(-1) === ".") {
-                    err.message = err.message.slice(0, -1);
-                  }
-                  err.message += ". Screenshot saved to \"" + ssFileName + "\"";
-                }
-                done(err);
-              });
-          };
           if (testsRun === 1) {
             // Otherwise WebDriverIO would complain that we define the same custom commands
             // multiple times
@@ -53,7 +80,7 @@ module.exports = {
             .deleteCookie()
             .then(function () {
               done();
-            }, errorHandler);
+            }, errorHandler(testName, client, done));
           });
       });
 
