8952bea9bb3077ac4ada2ea48cd443944031af4a
avernatter <avernatter@localhost>
2016-05-18 15:55:53 -0400

    CM get percent complete script update. Removed console output and set url to page to prevent redirect.

--- tests/jobs/PRD-11-caremeridian.js
+++ tests/jobs/PRD-11-caremeridian.js
@@ -23,7 +23,7 @@ execJobs({"PRD-11": function(client, done) {
 
 	.logInAs(user)
 
-	.then(function() {console.log("Logged in as an Administrator.."); })
+	//.then(function() {console.log("Logged in as an Administrator.."); })
 
 	//log in as Tina Richter
 	.url("https://cs4.salesforce.com/005U0000002AzvSIAS?noredirect=1&isUserEntityOverride=1")
@@ -31,26 +31,26 @@ execJobs({"PRD-11": function(client, done) {
 	.click("input[value=' Login ']")
 	.pause(5000)
 
-	.then(function() {console.log("..Performed a Login *As* Jessica Black."); })
-	.then(function() {console.log("...We have " + sa.length + " Service Assignment pages to process."); })
-	.then(function() {console.log("...We also have " + pbs_admissions.length + " PBS's and Admissions pages to process."); })
+	//.then(function() {console.log("..Performed a Login *As* Jessica Black."); })
+	//.then(function() {console.log("...We have " + sa.length + " Service Assignment pages to process."); })
+	//.then(function() {console.log("...We also have " + pbs_admissions.length + " PBS's and Admissions pages to process."); })
 
 
 	sa.forEach(function (a) {
 		client = client
-	     .then(function() {console.log("....Go to Service Assignment-->" + a.SAID); })
-	     .url("https://c.cs4.visual.force.com/" + a.SAID)
-	     .pause(2000)
+	     //.then(function() {console.log("....Go to Service Assignment-->" + a.SAID); })
+	     .url("https://c.cs4.visual.force.com/apex/ServiceAssignmentView?id=" + a.SAID)
+	     .pause(5000)
 	});
 
 	pbs_admissions.forEach(function (b) {
 		client = client
-	     .then(function() {console.log("....Go to PBS-->" + b.ContactID); })
-	     .url("https://c.cs4.visual.force.com/" + b.ContactID)
-	     .pause(2000)
-	     .then(function() {console.log("....Go to Admission-->" + b.AdmissionID); })
-	     .url("https://c.cs4.visual.force.com/" + b.AdmissionID)
-	     .pause(2000)
+	     //.then(function() {console.log("....Go to PBS-->" + b.ContactID); })
+	     .url("https://c.cs4.visual.force.com/apex/PersonBeingServedEditNew?id=" + b.ContactID + "&sfdc.override=1")
+	     .pause(5000)
+	     //.then(function() {console.log("....Go to Admission-->" + b.AdmissionID); })
+	     .url("https://c.cs4.visual.force.com/apex/AdmissionView?id=" + b.AdmissionID)
+	     .pause(5000)
 	});
 
 
