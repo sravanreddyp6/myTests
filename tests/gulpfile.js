5fd01bc2baf76314b5ac60ebcab642b368998642
Larry Tambasico <contractor.larry.tambascio@thementornetwork.com>
2016-06-20 12:05:14 -0400

    EIFB-27:Populating preferred name fields with regular name values if preferred fields are null.

---
+++ tests/gulpfile.js
@@ -0,0 +1,137 @@
+var argv = require('yargs')
+  .boolean('d')
+  .boolean('sd')
+  .boolean('users')
+  .alias('s', 'suite')
+  .alias('d', 'debug')
+  .alias('e', 'selenium-debug')
+  .alias('f', 'force')
+  .alias('j', 'file')
+  .alias('u', 'with-managed-users')
+  .argv;
+
+var findRemoveSync = require('find-remove');
+var execSync = require('child_process').execSync;
+var gulp = require('gulp');
+var selenium = require('selenium-standalone');
+var mocha = require('gulp-spawn-mocha');
+var path = require('path');
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var manageUsers = require('./users.js').manageUsers;
+
+gulp.task('selenium', function (done) {
+  var opts = {
+    version: '2.53.0',
+    baseURL: 'http://selenium-release.storage.googleapis.com',
+    drivers: {
+      chrome: {
+        version: '2.21',
+        arch: process.arch,
+        baseURL: 'http://chromedriver.storage.googleapis.com'
+      },
+      ie: {
+        version: '2.48.0',
+        arch: process.arch,
+        baseURL: 'http://selenium-release.storage.googleapis.com'
+     }
+    },
+    logger: function (message) {
+      if (argv["selenium-debug"]) {
+        console.log(message);
+      }
+    }
+  };
+  selenium.install(opts, function (err) {
+    if (err) return done(err);
+
+    selenium.start(opts, function (err, child) {
+      if (err) return done(err);
+      selenium.child = child;
+      done();
+    });
+  });
+});
+
+var inspector;
+gulp.task("inspector", function (done) {
+  inspector = spawn('node-inspector.cmd', [], {
+    detached: true,
+    stdio: [ 'ignore', 'ignore', 'ignore' ]
+  });
+  done();
+});
+
+gulp.task("manage-users", function (done) {
+  manageUsers(done, argv.force);
+});
+
+gulp.task("clean-up", function () {
+  findRemoveSync(path.resolve(__dirname, './screenshots'), { extensions: [ '.png' ] });
+});
+
+var cleanUpProcesses = function (err) {
+  console.log("Cleaning up processes");
+  var exitCode;
+  if (err) {
+    exitCode = 1;
+  }
+  selenium.child.kill();
+  if (argv.debug) {
+    var taskkill = spawn("taskkill", ["/pid", inspector.pid, '/f', '/t']);
+    taskkill.on("exit", function () {
+      process.exit(exitCode);
+    });
+  } else {
+    process.exit(exitCode);
+  }
+}
+
+var deps = ["clean-up", "manage-users", "selenium"];
+if (argv.debug) {
+  deps.push("inspector");
+}
+gulp.task("tests", deps, function () {
+  return gulp
+    .src(argv.suite ? argv.suite: 'suites/*.js', {read: false})
+    .pipe(mocha({
+      debugBrk: argv.debug
+    }))
+    .once('error', cleanUpProcesses)
+    .once('end', cleanUpProcesses);
+});
+
+var jobDeps = ["clean-up", "selenium"];
+if (argv.debug) {
+  jobDeps.push("inspector");
+}
+if (argv["with-managed-users"]) {
+  jobDeps.push("manage-users");
+}
+gulp.task("job", jobDeps, function () {
+  const execFile = function (fileLoc) {
+    const normalizedPath = path.normalize(fileLoc);
+    const errorStr = fileLoc + " is not a valid file";
+    if (!fs.existsSync(normalizedPath) || !fs.lstatSync(normalizedPath).isFile()) {
+      console.error(errorStr);
+      throw errorStr;
+    }
+    const nodeRuntime = argv.debug ? "node-debug" : "node";
+    execSync(nodeRuntime + " " + normalizedPath, {
+      stdio: [0, 1, 2]
+    });
+  };
+  try {
+    if (Array.isArray(argv.file)) {
+      argv.file.forEach(execFile);
+    } else if (!argv.file) {
+      throw "You have to specify at least one job file";
+    } else {
+      execFile(argv.file);
+    }
+  } catch (e) {
+    cleanUpProcesses(e);
+  } finally {
+    cleanUpProcesses();
+  }
+});
---
+++ tests/jobs/PRD-11-neuro.js
@@ -0,0 +1,60 @@
+var assert = require('chai').assert;
+var testSuite = require("../main.js").testSuite;
+var users = require("../users.js").accounts;
+var stripJsonComments = require("strip-json-comments");
+var fs   = require('fs');
+var auth = require("../configs/auth.json");
+var execJobs = require("../main").execJobs;
+
+
+var suiteTimeout = 10 * 60 * 1000;
+var defaultOperationTimeout = 2 * 60 * 1000;
+
+execJobs({"PRD-11": function(client, done) {
+        var sa = JSON.parse(stripJsonComments(fs.readFileSync("./jobs/json/all-neuro-sas.json", "utf8")));
+        var pbs_admissions = JSON.parse(stripJsonComments(fs.readFileSync("./jobs/json/all-neuro-pbs-admissions.json", "utf8")));
+
+	var user = {
+		username: "shaun.wood@thementornetwork.com.staging",
+		password: "Deploy!2345"	
+	};
+
+   client =  client
+
+	.logInAs(user)
+
+	.then(function() {console.log("Logged in as an Administrator.."); })
+
+	//log in as Tina Richter
+	.url("https://cs4.salesforce.com/005U0000003ASKBIA4?noredirect=1&isUserEntityOverride=1")
+	.pause(5000)
+	.click("input[value=' Login ']")
+	.pause(5000)
+
+	.then(function() {console.log("..Performed a Login *As* Jennifer Surprenant."); })
+	.then(function() {console.log("...We have " + sa.length + " Service Assignment pages to process."); })
+	.then(function() {console.log("...We also have " + pbs_admissions.length + " PBS's and Admissions pages to process."); })
+
+
+	sa.forEach(function (a) {
+		client = client
+	     .then(function() {console.log("....Go to Service Assignment-->" + a.SAID); })
+	     .url("https://c.cs4.visual.force.com/" + a.SAID)
+	     .pause(2000)
+	});
+
+	pbs_admissions.forEach(function (b) {
+		client = client
+	     .then(function() {console.log("....Go to PBS-->" + b.ContactID); })
+	     .url("https://c.cs4.visual.force.com/" + b.ContactID)
+	     .pause(2000)
+	     .then(function() {console.log("....Go to Admission-->" + b.AdmissionID); })
+	     .url("https://c.cs4.visual.force.com/" + b.AdmissionID)
+	     .pause(2000)
+	});
+
+
+	  return client;
+
+	}
+});
